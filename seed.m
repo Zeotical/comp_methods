3f97355e852739d824c10f33470577f93271fc81
Zeotical <aceyems10@gmail.com>
2025-06-17 06:39:59 +0800

    waiting time + time spent in system done

--- seed.m
+++ seed.m
@@ -13,7 +13,7 @@ printf('%s\n', choices);
 random_num_generator = input("Pick a random number generator: ") ;
 customers = input("Enter number of customers: ") ;
 disp(" ") ;
-disp ('Cust no   Rn ariiv   Inter arriv   Arriv time   Rn serv   Serv begin    Serv time     Serv ends     Time in system     Waiting time');
+disp ('Cust no   Rn ariiv   Inter arriv   Arriv time   Rn serv   Serv begin    Serv time     Serv ends     Waiting time     Time in system');
 
 % printing values to table + l
 for numofcust=1:customers
@@ -24,8 +24,10 @@ for numofcust=1:customers
      serv_begins = time_service_begins(Arrival_time,time_service_ends) ;
      Service_time = service_range(rn_service); %how long service is
      time_service_ends = Service_time + serv_begins ;
+     waiting_time = serv_begins - Arrival_time;
+     time_in_system = time_service_ends - Arrival_time;
      prev_arrival_time = Inter_arriv + prev_arrival_time ;
 
- fprintf('%2.0f %10d %10d %12d %14d %10d %10d %12d\n', [numofcust,rn_arrive,Inter_arriv, Arrival_time, rn_service, serv_begins, Service_time, time_service_ends]);
+ fprintf('%2.0f %10d %10d %12d %14d %10d %10d %14d %18d %14d\n', [numofcust,rn_arrive,Inter_arriv,Arrival_time, rn_service, serv_begins, Service_time, time_service_ends,waiting_time, time_in_system]);
 end
 
