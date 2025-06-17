81bf72c94a0e97a7e0cf74c5a6d407d01ca344ae
Zeotical <aceyems10@gmail.com>
2025-06-17 12:57:53 +0800

    values saved to matrix, each row ,col can be accessed , table displayed outside of for loop using/not using another for loop

--- seed.m
+++ seed.m
@@ -9,17 +9,17 @@ time_service_ends = 0;
 % later print one by one
 choices = ['1. Mixed LCG', '2. Multiplicative LCG', '3. Additive LCG'];
 printf('%s\n', choices);
-
+table_values = []; %clear the matrix after each run
 random_num_generator = input("Pick a random number generator doesnt do anythignyet: ") ;
 customers = input("Enter number of customers: ") ;
 disp(" ") ;
 disp ('Cust no   Rn ariiv   Inter arriv   Arriv time   Rn serv   Serv begin    Serv time     Serv ends     Waiting time     Time in system');
 %disp(" ");
 %disp ('TABLE 2   Rn ariiv   Inter arriv   Arriv time   Rn serv   Serv begin    Serv time     Serv ends     Waiting time     Time in system');
+  i = 0;
 
 % printing values to table + l
 for numofcust=1:customers
-  i = 0;
      rn_arrive = mixedlcg(rn_arrive,numofcust);
      Inter_arriv = arrive_range(rn_arrive);
      Arrival_time = Inter_arriv + prev_arrival_time ;
@@ -31,12 +31,14 @@ for numofcust=1:customers
      time_in_system = time_service_ends - Arrival_time;
      prev_arrival_time = Inter_arriv + prev_arrival_time ;
 
-     table_values = [numofcust,rn_arrive,Inter_arriv,Arrival_time, rn_service, serv_begins, Service_time, time_service_ends,waiting_time, time_in_system;] ;
+     table_values (numofcust,:) =  [numofcust rn_arrive Inter_arriv Arrival_time rn_service serv_begins Service_time time_service_ends waiting_time time_in_system];
 
- fprintf('%2.0f %10d %10d %12d %14d %10d %10d %14d %18d %14d\n', [table_values]);
 
  %fprintf('%2.0f %10d %10d %12d %14d %10d %10d %14d %18d %14d\n', [numofcust,rn_arrive,Inter_arriv,Arrival_time, rn_service, serv_begins, Service_time, time_service_ends,waiting_time, time_in_system]);
 
 end
  %fprintf('%2.0f %10d %10d %12d %14d %10d %10d %14d %18d %14d\n', [numofcust,rn_arrive,Inter_arriv,Arrival_time, rn_service, serv_begins, Service_time, time_service_ends,waiting_time, time_in_system]);
-
+%table_values
+for i=1:customers
+ fprintf('%2.0f %10d %10d %12d %14d %10d %10d %14d %18d %14d\n',  table_values(i,:));
+endfor
