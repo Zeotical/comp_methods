d9417dabd08dad56113aacbb0ff80ecd904c7ab2
Zeotical <aceyems10@gmail.com>
2025-06-20 10:47:41 +0800

    cleaning up code, disp + matrix to be handled next

--- seed.m
+++ seed.m
@@ -24,6 +24,9 @@ prev_service_end_time = 0;
 prev_service_end_time_line2 = 0;
 pump3 = 0;
 pump4 = 0;
+pump3_disp = "-" ;
+pump4_disp = "-" ;
+
 for numofcust=1:customers
 
      rn_arrive = mixedlcg(rn_arrive,numofcust);
@@ -39,45 +42,98 @@ for numofcust=1:customers
      line_num = line_numm(numofcust);
 
 
-     % Line 1
+     % Line 1 pump 1
      if (line_num == 1 && Arrival_time > prev_service_end_time)
 
-     table_values (numofcust,:) =  [numofcust pseudo  pseudo pseudo  rn_arrive Inter_arriv Arrival_time line_num rn_service serv_begins Service_time time_service_ends];
+       p1_serv_begins = serv_begins ;
+       p1_Service_time = Service_time ;
+       p1_time_service_ends = time_service_ends ;
 
-     fprintf('%2.0f %13d %13d %12d %12d %12d %12d %12d %12d %10d %10d %10d\n', [numofcust,pseudo,pseudo,pseudo,rn_arrive,Inter_arriv,Arrival_time, line_num, rn_service, serv_begins, Service_time, time_service_ends]);
-     table (numofcust,:) = [numofcust pump3 pump3 pump4 pump3 pump3 pump3 pump4 pump4 pump4 waiting_time time_in_system ];
-     %table_values (:,5);
-     prev_service_end_time = time_service_ends;
+       p2_serv_begins = 0 ;
+       p2_Service_time = 0 ;
+       p2_time_service_ends = 0 ;
 
+       p3_serv_begins = 0 ;
+       p3_Service_time = 0 ;
+       p3_time_service_ends = 0 ;
+       prev_service_end_time = 0 ;
+
+       p4_serv_begins = 0 ;
+       p4_Service_time = 0 ;
+       p4_time_service_ends = 0 ;
+
+       prev_service_end_time = time_service_ends;
+
+      % Line 1, pump 2
      elseif(line_num == 1 && Arrival_time < prev_service_end_time)
 
+       p2_serv_begins = serv_begins ;
+       p2_Service_time = Service_time ;
+       p2_time_service_ends = time_service_ends ;
+       prev_service_end_time = time_service_ends;
 
-      %deal with table one can jus access matrix rows and cols to add what I want here no need all this yapping
-       table_values (numofcust,:) =  [numofcust pseudo  pseudo pseudo  rn_arrive Inter_arriv Arrival_time line_num rn_service pseudo pseudo pseudo];
-       fprintf('%2.0f %13d %13d %12d %12d %12d %12d %12d %12d %10d %10d %10d\n', [numofcust,pseudo,pseudo,pseudo,rn_arrive,Inter_arriv,Arrival_time, line_num, rn_service, pseudo, pseudo, pseudo]);
+       p1_serv_begins = 0 ;
+       p1_Service_time = 0 ;
+       p1_time_service_ends = 0 ;
 
-      table (numofcust,:) = [numofcust serv_begins Service_time time_service_ends pump3 pump3 pump3 pump4 pump4 pump4 waiting_time time_in_system ];
-      prev_service_end_time = time_service_ends;
-      % Line 2
+       p3_serv_begins = 0 ;
+       p3_Service_time = 0 ;
+       p3_time_service_ends = 0 ;
+
+       p4_serv_begins = 0 ;
+       p4_Service_time = 0 ;
+       p4_time_service_ends = 0 ;
+
+      % Line 2, pump 3
      elseif (line_num == 2 && Arrival_time > prev_service_end_time_line2)
 
-    %deal with table one can jus access matrix rows and cols to add what I want here no need all this yapping
-       table_values (numofcust,:) =  [numofcust pseudo  pseudo pseudo  rn_arrive Inter_arriv Arrival_time line_num rn_service pseudo pseudo pseudo];
-       fprintf('%2.0f %13d %13d %12d %12d %12d %12d %12d %12d %10d %10d %10d\n', [numofcust,pseudo,pseudo,pseudo,rn_arrive,Inter_arriv,Arrival_time, line_num, rn_service, pseudo, pseudo, pseudo]);
+      p3_serv_begins = serv_begins ;
+      p3_Service_time = Service_time ;
+      p3_time_service_ends = time_service_ends ;
+      prev_service_end_time_line2 = time_service_ends;
+
+       p1_serv_begins = 0 ;
+       p1_Service_time = 0 ;
+       p1_time_service_ends = 0 ;
+
+       p2_serv_begins = 0 ;
+       p2_Service_time = 0 ;
+       p2_time_service_ends = 0 ;
+
+       p4_serv_begins = 0 ;
+       p4_Service_time = 0 ;
+       p4_time_service_ends = 0 ;
+
+       cell = {numofcust,pseudo,pseudo,pseudo,rn_arrive,Inter_arriv,Arrival_time, line_num, rn_service,hi, hi, hi};
+       fprintf('%2.0f %13d %13d %12d %12d %12d %12d %12d %12d %10s %10d %10d\n', cell{:});
 
       table (numofcust,:) = [numofcust  pump3 pump3 pump3 serv_begins Service_time time_service_ends pump4 pump4 pump4 waiting_time time_in_system ];
-      prev_service_end_time_line2 = time_service_ends;
-
+      % Line 2, pump 4
      elseif(line_num == 2 && Arrival_time < prev_service_end_time_line2)
 
+         p4_serv_begins = serv_begins ;
+         p4_Service_time = Service_time ;
+         p4_time_service_ends = time_service_ends ;
+         prev_service_end_time_line2 = time_service_ends;
 
-      %deal with table one can jus access matrix rows and cols to add what I want here no need all this yapping
-       table_values (numofcust,:) =  [numofcust pseudo  pseudo pseudo  rn_arrive Inter_arriv Arrival_time line_num rn_service pseudo pseudo pseudo];
+       p1_serv_begins = 0 ;
+       p1_Service_time = 0 ;
+       p1_time_service_ends = 0 ;
+
+       p2_serv_begins = 0 ;
+       p2_Service_time = 0 ;
+       p2_time_service_ends = 0 ;
+
+       p3_serv_begins = 0 ;
+       p3_Service_time = 0 ;
+       p3_time_service_ends = 0 ;
+
+endif
+       table_values (numofcust,:) =  [numofcust pseudo  pseudo pseudo  rn_arrive Inter_arriv Arrival_time line_num rn_service p1_serv_begins p1_Service_time p1_time_service_ends];
        fprintf('%2.0f %13d %13d %12d %12d %12d %12d %12d %12d %10d %10d %10d\n', [numofcust,pseudo,pseudo,pseudo,rn_arrive,Inter_arriv,Arrival_time, line_num, rn_service, pseudo, pseudo, pseudo]);
 
-      table (numofcust,:) = [numofcust  pump3 pump3 pump3 pump4 pump4 pump4 serv_begins Service_time time_service_ends waiting_time time_in_system ];
-      prev_service_end_time_line2 = time_service_ends;
-endif
+      table (numofcust,:) = [numofcust p2_serv_begins p2_Service_time p2_time_service_ends p3_serv_begins p3_Service_time p3_time_service_ends p4_serv_begins p4_Service_time p4_time_service_ends waiting_time time_in_system ];
+
 endfor
 
 %Dealing with second part of the table
