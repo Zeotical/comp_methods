3243966e5460b8656422532f08ad2b31677ad4d5
Zeotical <aceyems10@gmail.com>
2025-06-22 07:50:58 +0800

    Petrol range

--- petrol_type.m
+++ petrol_type.m
@@ -1,13 +1,13 @@
 function [petrol_num,petrol,price_per_litre] = petrol_type(rn_petrol)
-    if(0<=rn_petrol && rn_petrol <=57)
+    if(0<=rn_petrol && rn_petrol <=58)
         petrol_num = 1;
         petrol = "RON95";
         price_per_litre = 2.05;
-    elseif (58<=rn_petrol && rn_petrol <=77)
+    elseif (59<=rn_petrol && rn_petrol <=78)
         petrol_num = 2;
         petrol = "RON97";
         price_per_litre = 3.14;
-    elseif (78<=rn_petrol && rn_petrol <=81)
+    elseif (79<=rn_petrol && rn_petrol <=81)
         petrol_num = 3;
         petrol = "RON100";
         price_per_litre = 5.00;
